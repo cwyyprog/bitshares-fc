bbe6ec1f593649f563436a358c0b37b5f00462cc
Qixing <cwyyprog@163.com>
2019-05-07 18:54:46 +0800

    build with boost 1.60, not 1.70

--- appveyor.bash
+++ appveyor.bash
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
+set -e
 export MSYSTEM=MINGW64
-pacman --noconfirm -S  mingw-w64-x86_64-boost; #mingw-w64-x86_64-openssl
+wget http://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-boost-1.60.0-4-any.pkg.tar.xz
+pacman --noconfirm -U mingw-w64-x86_64-boost-1.60.0-4-any.pkg.tar.xz
+pacman --noconfirm -S mingw-w64-x86_64-ccache
+#pacman --noconfirm -S  mingw-w64-x86_64-boost; #mingw-w64-x86_64-openssl
 mkdir /msys64
-mount C:/msys64 /msys64;
-cd $APPVEYOR_BUILD_FOLDER/bitshares-fc;
-cmake -G "MSYS Makefiles" . -DOPENSSL_CONF_SOURCE="C:/msys64/mingw64/ssl/openssl.cnf" -DOPENSSL_ROOT_DIR=/mingw64 -DBOOST_ROOT=/mingw64;
-#cmake --build . -j 2 --target all_tests;
-cmake --build . -j 2 --target log_test;
-cmake --build . -j 2 --target bloom_test;
-cmake --build . -j 2 --target hmac_test;
-cmake --build . -j 2 --target blind;
-cmake --build . -j 2 --target task_cancel_test;
-#cmake --build . -j 2 --target api;
-cmake --build . -j 2 --target real128_test;
-cmake --build . -j 2 --target ecc_test;
-cmake --build . -j 2 --target bip_lock;
+mount C:/msys64 /msys64
+cd $APPVEYOR_BUILD_FOLDER/bitshares-fc
+ccache -s
+cmake -G "MSYS Makefiles" . -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DOPENSSL_CONF_SOURCE="C:/msys64/mingw64/ssl/openssl.cnf" -DOPENSSL_ROOT_DIR=/mingw64 -DBOOST_ROOT=/mingw64
+cmake --build . --config Release -j 3
+ccache -s
 echo "Build finished!"
+tests/hmac_test 2>&1 | cat
+tests/ecc_test README.md 2>&1 | cat
+tests/log_test 2>&1 | cat
